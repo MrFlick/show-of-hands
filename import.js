141486595d29e0eb0a4cd10499947f9de3db69fc
Matthew Flickinger <mrflick@gmail.com>
2019-06-20 18:55:26 -0400

    Add import script

---
+++ import.js
@@ -0,0 +1,53 @@
+// Script to import snippets
+
+// Snippets should start with a line that begins with "##" and
+// the srting after that will be used as the name. The rows until
+// the next "##" line are used as the code. Leading/trailing blank
+// lines are stripped out
+
+const config = require('./config')
+const fs = require('fs')
+const data = require("./data-layer").getDataStore(config.db_path)
+
+if ( process.argv.length !==3 ) {
+    console.error('Expected filename to import!')
+    process.exit(1)
+}
+
+const filename = process.argv[2]
+
+async function addItem(title, body) {
+    return data.addSnippet({title: title, code: body})
+}
+
+
+function importFile(filename) {
+    fs.readFile(filename, 'utf8', function(err, contents) {
+    const lines = contents.split(/\n/)
+    let body = [];
+    let title = ""
+    async function flush() {
+        while (body.length && body[0]=="") {
+            body.shift();
+        }
+        while (body.length && body[body.length-1]=="") {
+            body.pop();
+        }
+        if (body.length) {
+            await addItem(title, body.join("\n"));
+        }
+        body = [];
+        title = "";
+    }
+    lines.forEach((line) => {
+        if (line.startsWith("##")) {
+            flush();
+            title = line.substring(2).trim()
+        } else {
+            body.push(line)
+        }
+    })
+    flush();
+})
+}
+importFile(filename);
